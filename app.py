b80a8bedf84ada0c93305c78326caf649e46eabc
DanielLedvin <114612747+DanielLedvin@users.noreply.github.com>
2025-05-26 19:29:25 -0700

    Added files for app

---
+++ app.py
@@ -0,0 +1,183 @@
+import streamlit as st
+import geopandas as gpd
+import pandas as pd
+from streamlit_folium import st_folium
+import json
+import visualizations as viz
+
+
+# --- To Run in Terminal ---
+# streamlit run interactive_map/app_formatted.py
+
+
+# --- Page Configuration ---
+st.set_page_config(layout='wide',
+                   page_title='Sub-Saharan Crop Forecast Viewer')
+st.title('Sub-Saharan Africa Crop Forecast Viewer')
+
+
+# --- Data Loading Function ---
+@st.cache_data
+def load_data():
+    '''
+    Function:
+    - Reads in cape_eo and crop production datasets as well as shapefile for mapping
+    - Formats DataFrames for use in the interactive map
+
+    Return:
+    - cape_eo = Cape EO observations
+    - viewer_yield = Yield values for Maize. columns are only fnid, year, and yield
+    - gdf = Shapefile for mapping Sub-Saharan Africa
+
+    Note:
+    - cape_eo predictors are the same for the XGB and LR models, so I arbitrarily chose
+        the rows from the XGB model not to have repeating predictor values
+    '''
+    # Read in files from 'data/' folder
+    cape_eo = pd.read_parquet('./data/cape_eo_formatted.parquet')
+    viewer_yield = pd.read_parquet('./data/viewer_yield.parquet')
+    yield_fcst = pd.read_parquet('./data/viewer_yield_fcst_error.parquet')
+
+    # Load gdf for lookups
+    gdf = gpd.read_file('./data/gscd_shape_simplified.gpkg',
+                        layer='gscd_shape_simplified',
+                        engine='pyogrio')
+
+    # Load GeoJSON
+    with open('./data/gscd_shape.geojson') as f:
+        geojson = json.load(f)
+
+    return cape_eo, viewer_yield, yield_fcst, gdf, geojson
+
+
+# --- Load Data ---
+cape_eo, viewer_yield, yield_fcst, gdf, geojson = load_data()
+eo_vars = ['ndvi', 'prcp', 'pdry', 'etos', 'tavg', 'gdd', 'kdd']
+
+
+# --- Page Selection ---
+page = st.sidebar.radio("Page", ["Yield & Climate", "Historical Forecasts"])
+
+
+# --- Page 1: Current Conditions ---
+if page == "Yield & Climate":
+
+    # --- Build Map ---
+    st.markdown('### Click a region to view EO and crop data')
+
+    m = viz.build_base_map(geojson)
+    map_output = st_folium(m, width=900, height=600)
+
+    fnid = None
+    
+    # --- Display Data ---
+    if map_output and map_output.get('last_active_drawing'):
+        fnid = map_output['last_active_drawing']['properties']['fnid']
+
+        # Use gdf for lookup
+        region_row = gdf[gdf['fnid'] == fnid].iloc[0]
+        region_name = f"{region_row['admin0']} - {region_row['admin1']}"
+        st.success(f'Selected Region: {region_name}')
+
+
+    if fnid:
+        # Filter by region
+        crop_yield = viewer_yield[viewer_yield['fnid'] == fnid]
+        eo_df = cape_eo[cape_eo['fnid'] == fnid]
+
+        if not eo_df.empty:
+            # Select what EO to display
+            selected_eo_var = st.selectbox(
+                label='Select EO variables to plot',
+                options=eo_vars,
+                index=0
+            )
+        
+        yield_col, eo_col = st.columns(2)
+
+        # Yield graph
+        with yield_col:
+            if crop_yield.empty:
+                st.warning('No yield data available for this region.')
+            else:
+                fig_yield = viz.plot_yield_obs(crop_yield)
+                st.pyplot(fig_yield)
+
+        # EO predictor graph
+        with eo_col:
+            if not eo_df.empty:
+                fig_eo = viz.plot_eo_var(eo_df, selected_eo_var)
+                st.pyplot(fig_eo)
+            else:
+                st.warning('No EO data available for this region.') 
+
+    else:
+        st.info('Click a region on the map to begin.')
+
+
+# --- Page 2: Historical Forecasts ---
+if page== "Historical Forecasts":
+    st.markdown("### View Historical Forecast and Error")
+
+    if "choropleth_fig" not in st.session_state:
+        st.session_state.choropleth_fig = None
+
+    var_col, country_col, season_col, years_col, model_col, lead_col = st.columns(6)
+
+    with var_col:
+        variables = [col for col in yield_fcst.columns if col not in ['fnid', 'country', 'admin1', 'admin2', 'season', 'year', 'model', 'lead']]
+        selected_variable = st.selectbox('Variable', variables)
+    
+    with country_col:
+        # Country selection
+        countries = sorted(yield_fcst['country'].unique())
+        selected_country = st.selectbox('Country', countries)
+        country_fnids = yield_fcst[yield_fcst['country'] == selected_country]['fnid'].unique().tolist()
+
+    with season_col:
+        # Select season
+        seasons = yield_fcst[yield_fcst['fnid'].isin(country_fnids)]['season'].unique()
+        selected_season = st.selectbox('Season', seasons)
+
+    with years_col:
+        # Find available years for country fnids
+        yrs = yield_fcst.loc[((yield_fcst['fnid'].isin(country_fnids)) 
+                              & (yield_fcst['season']==selected_season)), 
+                              'year'].dropna()
+        if yrs.empty:
+            st.warning("No forecast years available for the selected conditions.")
+            st.stop()
+        min_year, max_year = int(yrs.min()), int(yrs.max())
+        year_range = list(range(min_year, max_year + 1))
+        selected_year = st.selectbox('Year', year_range)
+
+    # Parameter selection
+    with model_col:
+        models = sorted(yield_fcst["model"].unique())
+        selected_model = st.selectbox("Model", models)
+
+    with lead_col:
+        leads  = sorted(yield_fcst["lead"].unique())
+        selected_lead  = st.selectbox("Lead",  leads)
+
+    # --- Create Apply Button ---
+    update = st.button("Update Map")
+
+    if update:
+
+        fig = viz.create_map(yield_fcst, 
+                             geojson, 
+                             gdf,
+                             selected_variable,
+                             country_fnids, 
+                             selected_country,
+                             selected_season, 
+                             selected_year, 
+                             selected_model, 
+                             selected_lead)
+        
+        st.session_state.choropleth_fig = fig
+
+    if st.session_state.choropleth_fig is not None:
+        st.plotly_chart(st.session_state.choropleth_fig, use_container_width=True)
+
---
+++ visualizations.py
@@ -0,0 +1,137 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
+import folium
+import streamlit as st
+
+
+@st.cache_resource
+def build_base_map(geojson):
+
+    m = folium.Map(location=[0, 20], zoom_start=3)
+
+    # Draw map from GeoJSON
+    folium.GeoJson(
+        geojson,
+        tooltip=folium.GeoJsonTooltip(
+            fields=['admin0', 'admin1', 'admin2'],
+            aliases=['Country: ', 'admin1: ', 'admin2:'],
+            localize=True
+        )
+    ).add_to(m)
+
+    return m
+
+
+@st.cache_resource
+def plot_yield_obs(crop_yield):
+
+    fig, ax = plt.subplots()
+
+    sns.lineplot(
+        data=crop_yield,
+        x='year',
+        y='yield_obs',
+        ax=ax,
+        marker='o'
+    )
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Yield (t/ha)')
+    ax.set_title('Maize Yield Over Time')
+    fig.tight_layout()
+    
+    return fig
+
+@st.cache_resource
+def plot_eo_var(eo_df, selected_eo):
+
+    fig, ax = plt.subplots()
+    sns.lineplot(
+        data=eo_df,
+        x='date',
+        y=selected_eo,
+        ax=ax,
+        marker='o'
+    )
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel(selected_eo)
+    ax.set_title(f'{selected_eo} Over Time (Pre-Harvest)')
+    fig.autofmt_xdate()
+    fig.tight_layout()
+
+    return fig
+
+
+@st.cache_resource
+def create_map(yield_fcst, geojson, _gdf, selected_variable, country_fnids, selected_country, selected_season, selected_year, selected_model, selected_lead):
+
+    # Dictionary for mapping variables:
+    label_map = {
+        "yield_fcst":         ("Forecast Yield",    "t/ha"),
+        "yield_fcst_error":   ("Forecast Error",    "t/ha"),
+        "yield_fcst_perror":  ("Percent Error",     "%"),
+    }
+
+    var_label, var_unit = label_map.get(selected_variable, (selected_variable, ""))
+    
+    # Filter for selection
+    df_filt = yield_fcst.query(
+        "fnid in @country_fnids " 
+        "& season==@selected_season "
+        "& year==@selected_year "
+        "& model==@selected_model "
+        "& lead==@selected_lead",
+        engine="python"
+    )
+
+    # --- Build two-layer map ---
+    # Layer 1: color every region gray in case of missing data
+    # Layer 2: Overlay only region with data
+
+    fig = go.Figure()
+
+    # # --- Layer 1 ---
+    # fig.add_trace(go.Choroplethmapbox(
+    #     geojson=geojson,
+    #     locations=country_fnids,
+    #     z=[0]*len(country_fnids),
+    #     featureidkey="properties.fnid",
+    #     colorscale=[[0, "lightgray"], [1, "lightgray"]],
+    #     showscale=False,
+    #     marker_line_width=0.5,
+    #     marker_line_color="black"
+    # ))
+
+    # --- Layer 2 ---
+    if not df_filt.empty:
+        fig.add_trace(go.Choroplethmapbox(
+            geojson=geojson,
+            locations=df_filt["fnid"],
+            z=df_filt[selected_variable],
+            featureidkey="properties.fnid",
+            colorscale="RdYlBu",
+            marker_line_width=0.5,
+            colorbar_title=var_label,
+            text=df_filt['admin1'],
+            hovertemplate=(
+                "<b>%{text}</b><br>"
+                f"{var_label}: "+"%{z:.2f} "+var_unit+"<extra></extra>"
+            )
+        ))
+    
+    # --- Layout ---
+    # center on the country’s centroid
+    country_gdf = _gdf[_gdf["admin0"] == selected_country]
+    lat0 = country_gdf.centroid_lat.mean()
+    lon0 = country_gdf.centroid_lon.mean()
+
+    fig.update_layout(
+        mapbox_style="carto-positron",
+        mapbox_zoom=4,
+        mapbox_center={"lat": lat0, "lon": lon0},
+        margin={"r":0,"t":0,"l":0,"b":0}
+    )
+
+    return fig
